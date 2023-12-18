6cf0e6ea9c4ace6596cdfbbe2b5a1c4251c3ac9c
Chris <cjcotton88@gmail.com>
2023-12-18 16:11:00 +1300

    Update SetupGrub.sh

--- SetupGrub.sh
+++ SetupGrub.sh
@@ -3,7 +3,7 @@
 echo "Grub Setup for EFI!"
 echo "Specify your efi directory | Type '/boot/efi' if unsure!"
 read EFIDIRECTORY
-arch-chroot grub-install --target=x86_64-efi --efi-directory=$EFIDIRECTORY --bootloader-id=grub_uefi --removable
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=$EFIDIRECTORY --bootloader-id=grub_uefi --removable
 sleep 1
 echo "Seting up our Grub Config:"
-arch-chroot grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
