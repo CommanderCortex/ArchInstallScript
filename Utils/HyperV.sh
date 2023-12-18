9d306d4790f9e315dc8bbe7cc1c318efbe285408
BuildTools <unconfigured@null.spigotmc.org>
2023-12-18 23:36:47 +1300

    Added Comments

--- Utils/HyperV.sh
+++ Utils/HyperV.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo "Finds the ID of our Root Partition"
-echo "Enter device location e.g. </dev/sda3> | if you followed the setup process in SetupDisks.sh it will be /dev/sda3 by default"
-read DEVICE
-uuid=$(blkid --match-tag UUID | grep $DEVICE) && uuid1="${uuid#*=}"
-echo $uuid1
+echo "Setup an EFI File for Booting in Hyper-V"
+lsblk
+echo "Enter our root partition here - e.g. </dev/sda3> | use our /mnt Partition will either be /dev/sda3 or /dev/nvme0n1p3"
+read DEVICE #Gets the input of our Device so we can grab the UUID (Unique Identifier) of the device
+uuid=$(blkid --match-tag UUID | grep $DEVICE) && uuid1="${uuid#*=}" #Grabs the Exact UUID of our device using blkid (Block ID)
+echo $uuid1 #Prints Back the UUID of our Device
 
-arch-chroot /mnt pacman -S efibootmgr --noconfirm
+arch-chroot /mnt pacman -S efibootmgr --noconfirm #Installs efibootmgr (Efi Boot Manager) - This allows us to install a bootloader for Hyper-V to boot
 arch-chroot /mnt efibootmgr --disk /dev/sda --part 1 --create --label "Linux Kernel" --loader /vmlinuz-linux --verbose \ --unicode 'root=PARTUUID=$uuid1 rw initrd=\initramfs-linux.img' 
-echo "Efi Boot Manager Enabled via EFI File for Hyper-V will poweroff in 3s! Just start the VM normally after poweroff"
+#The command above creates a label in our EFI File names Linux Kernel which tells the boot loader to boot into linux from our UUID we set before.
+echo "Efi Boot Manager Enabled via EFI File for Hyper-V | Will power off in 3s Just start the VM normally after device powers off"
 sleep 3
 poweroff
-
--- Utils/Wifi.sh
+++ Utils/Wifi.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 #MADE BY www.github.com/CommanderCortex ALL RIGHTS RESERVED
-#Calls our Script function in BASH
 
-# $ allows us to call saved data in varible form
 clear
-ip link #Shows our interfaces name:
-echo "lo is always our localhost ignore this interface"
+ip link #Shows all of our Network interfaces:
+echo "lo is our loopback device"
 echo "This Script assumes you're using Wifi, if you are using Ethernet cancel this script"
 
 echo " "
@@ -14,7 +12,7 @@ echo "Make sure this script is run as root or with sudo!"
 echo ""
 #-------------------------------------------------------------------------------------#
 echo "Input Network Device Name (e.g <wlan0>):" #Sets up our Networking interface.
-read INTERFACE #Saves the name of our interface in the $INTERFACE varible.
+read INTERFACE #Saves the name of our interface in the $INTERFACE variable.
 ip link set $INTERFACE up #Uses ip link to ensure our device is powered on.
 #-------------------------------------------------------------------------------------#
 iwctl device $INTERFACE set-property Powered on #Ensures our Device is Powered on
@@ -28,15 +26,13 @@ read SSID #Captures the desired SSID the user wants to connect to.
 echo "Enter Password for $SSID"
 read PASSWORD #Captues the Password for the Network.
 #-------------------------------------------------------------------------------------#
-iwctl --passphrase $PASSWORD station wlan0 connect $SSID
+iwctl --passphrase $PASSWORD station wlan0 connect $SSID #Connects to our Network with the given SSID & Password
 iwctl station list
 echo "Ensure your device reads 'Connected' then run 'ping archlinux.org' to ensure you're connected!"
 #End of Script.
 #If you face issues with your adapter then change <phy0> to the name of your adapter:
 #To find your adapters name use iwctl adapter list
-#
 #If you're having issues with your network device not showing up in 'ip link' then 'lspci' to list your PCI devices, if its missing then you need to install the firmware for your network device
-#
-#
+
 
 
