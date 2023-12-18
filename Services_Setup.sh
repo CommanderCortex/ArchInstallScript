ac4142da97f614fb9fc7aaa41e46be9d86eb438e
Chris <cjcotton88@gmail.com>
2023-12-18 17:17:39 +1300

    Update Services_Setup.sh

--- Services_Setup.sh
+++ Services_Setup.sh
@@ -2,7 +2,10 @@
 
 # systemctl enable creates a flag that starts services at boot
 # the --now flag starts the service staright away
+echo -e "\nEnabling essential services"
 
-systemctl enable NetworkManager --now #Example: Enables our Network Manager on boot.
-systemctl enable iwd --now
+systemctl enable NetworkManager.service #Example: Enables our Network Manager on boot.
+systemctl enable bluetooth
+systemctl enable iwd 
+
 #Add any services here that you install to start on boot
