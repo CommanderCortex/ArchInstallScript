dabe5aa4715debba98d0697510da5f806ebefeda
Chris <cjcotton88@gmail.com>
2023-12-18 18:44:01 +1300

    Update HyperV.sh

--- HyperV.sh
+++ HyperV.sh
@@ -3,12 +3,11 @@
 echo "Finds the ID of our Root Partition"
 echo "Enter device location e.g. </dev/sda3> | if you followed the setup process in SetupDisks.sh it will be /dev/sda3 by default"
 read DEVICE
-blkid $DEVICE
-echo "Type our PARTUUID here:"
-read UUID
+uuid=$(blkid --match-tag UUID | grep $DEVICE) && uuid1="${uuid#*=}"
+echo $uuid1
 
 arch-chroot /mnt pacman -S efibootmgr --noconfirm
-arch-chroot /mnt efibootmgr --disk /dev/sda --part 1 --create --label "Linux Kernel" --loader /vmlinuz-linux --verbose \ --unicode 'root=PARTUUID=$UUID rw initrd=\initramfs-linux.img' 
+arch-chroot /mnt efibootmgr --disk /dev/sda --part 1 --create --label "Linux Kernel" --loader /vmlinuz-linux --verbose \ --unicode 'root=PARTUUID=$uuid1 rw initrd=\initramfs-linux.img' 
 
 echo "Reboot now with 'reboot'"
 
