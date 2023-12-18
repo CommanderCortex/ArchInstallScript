9ebfa6e9d13bda81f72314c740cc8b7f4c4c42c9
Chris <cjcotton88@gmail.com>
2023-12-18 16:18:25 +1300

    Update SetupGrubForVM.sh

--- SetupGrubForVM.sh
+++ SetupGrubForVM.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+echo "Finds the ID of our Root Partition"
+echo "Enter device location e.g. </dev/sda3> | if you followed the setup process in SetupDisks.sh it will be /dev/sda3 by default"
+read DEVICE
+blkid $DEVICE
+echo "Type our PARTUUID here:"
+read UUID
+efibootmgr --disk /dev/sda --part 1 --create --label "Linux Kernel" --loader /vmlinuz-linux --verbose \ --unicode 'root=PARTUUID=$UUID rw initrd=\initramfs-linux.img' 
+
+echo "Reboot now with 'reboot'"
 
