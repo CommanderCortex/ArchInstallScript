8500669a6db0daebebff9b800efa18c8b2ee6921
Chris <cjcotton88@gmail.com>
2023-12-18 17:56:45 +1300

    Update DesktopEnvironment_Setup.sh

--- DesktopEnvironment_Setup.sh
+++ DesktopEnvironment_Setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-#Run this script after installing everything and booting into Arch
+#Run this script after installing everything
 
 arch-chroot /mnt pacman -Syu
 arch-chroot /mnt pacman -S alacritty sddm wayland plasma-desktop
 arch-chroot /mnt systemctl enable sddm 
+
+reboot
