10dbd107b8817cefe332b0ed6717adb323615a03
Chris <CommanderCortex@proton.me>
2024-01-08 17:06:41 +1300

    Updated Wifi Script
    Added Conditions

--- Utils/Wifi.sh
+++ Utils/Wifi.sh
@@ -13,7 +13,12 @@ echo ""
 #-------------------------------------------------------------------------------------#
 echo "Input Network Device Name (e.g <wlan0>):" #Sets up our Networking interface.
 read INTERFACE #Saves the name of our interface in the $INTERFACE variable.
-ip link set $INTERFACE up #Uses ip link to ensure our device is powered on.
+if [ -z "$INTERFACE" ]; then 
+	echo "Error, Device cannot be Null!"
+	exit
+else
+	ip link set $INTERFACE up; #Uses ip link to ensure our device is powered on.
+fi
 #-------------------------------------------------------------------------------------#
 iwctl device $INTERFACE set-property Powered on #Ensures our Device is Powered on
 iwctl adapter phy0 set-property Powered on #Ensures our Adapter is Powered on
@@ -29,6 +34,7 @@ read PASSWORD #Captues the Password for the Network.
 iwctl --passphrase $PASSWORD station wlan0 connect $SSID #Connects to our Network with the given SSID & Password
 iwctl station list
 echo "Ensure your device reads 'Connected' then run 'ping archlinux.org' to ensure you're connected!"
+#-------------------------------------------------------------------------------------#
 #End of Script.
 #If you face issues with your adapter then change <phy0> to the name of your adapter:
 #To find your adapters name use iwctl adapter list
